96c4bdb96c939ed9f57a229a051fea966767b3ff
Spedry <spedrysk@gmail.com>
2021-10-19 22:04:27 +0200

    added Platform.runLater when updating wcmAnimeEntryObservableList

--- src/main/java/sk/spedry/weebcollector/work/WCWorkPlace.java
+++ src/main/java/sk/spedry/weebcollector/work/WCWorkPlace.java
@@ -1,19 +1,16 @@
 package sk.spedry.weebcollector.work;
 
 import com.google.gson.Gson;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import sk.spedry.weebcollector.app.controllers.AddNewAnimeController;
 import sk.spedry.weebcollector.app.controllers.PreferencesPopupController;
 import sk.spedry.weebcollector.app.controllers.WeebCollectorController;
-import sk.spedry.weebcollector.app.controllers.choiceboxitems.CodeTable;
 import sk.spedry.weebcollector.app.controllers.util.WCMAnimeEntry;
-import sk.spedry.weebcollector.app.controllers.util.WCMServer;
 import sk.spedry.weebcollector.app.controllers.util.WCMSetup;
 import sk.spedry.weebcollector.app.controllers.util.WCMessage;
 import sk.spedry.weebcollector.app.controllers.util.lists.AnimeList;
-import sk.spedry.weebcollector.app.controllers.util.lists.ServerList;
 
 public class WCWorkPlace {
 
@@ -23,11 +20,12 @@ public class WCWorkPlace {
 
     public void addNewAnimeEntry(WCMessage wcMessage) {
         final ObservableList<WCMAnimeEntry> wcmAnimeEntryObservableList = WeebCollectorController.wcmAnimeEntryObservableList;
-        wcmAnimeEntryObservableList.clear();
-        wcmAnimeEntryObservableList.add(new WCMAnimeEntry("Test Anime Entry Test Anime Entry", "1080p", "Bot", "24", "8"));
-        AnimeList animeList = new Gson().fromJson(wcMessage.getMessageBody(), AnimeList.class);
-        wcmAnimeEntryObservableList.addAll(animeList.getAnimeList());
-        for (WCMAnimeEntry animeEntry : animeList.getAnimeList()) {
+        Platform.runLater(() -> {
+            wcmAnimeEntryObservableList.clear();
+            AnimeList animeList = new Gson().fromJson(wcMessage.getMessageBody(), AnimeList.class);
+            wcmAnimeEntryObservableList.addAll(animeList.getAnimeList());
+        });
+        for (WCMAnimeEntry animeEntry : wcmAnimeEntryObservableList) {
             logger.info(animeEntry.getAnimeName());
         }
     }
