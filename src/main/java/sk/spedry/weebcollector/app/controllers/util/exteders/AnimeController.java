7c7600d13a6a284f045f5cd853a9f1e3e1d8a523
Spedry <spedrysk@gmail.com>
2021-10-19 21:53:46 +0200

    anime controller for EditAnimeController.java and AddNewAnimeController.java

---
+++ src/main/java/sk/spedry/weebcollector/app/controllers/util/exteders/AnimeController.java
@@ -0,0 +1,65 @@
+package sk.spedry.weebcollector.app.controllers.util.exteders;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sk.spedry.weebcollector.app.controllers.choiceboxitems.CodeTable;
+import sk.spedry.weebcollector.clienthandler.ClientMessageSender;
+
+import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AnimeController extends ClientMessageSender implements Initializable {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @FXML
+    public TextField animeNameTextField;
+    @FXML
+    public TextField botTextField;
+    @FXML
+    public ChoiceBox<CodeTable> qualityChoiceBox;
+    @FXML
+    public TextField numberOfEpisodesTextField;
+
+    public final ObservableList<CodeTable> qualityTypeObservableList = FXCollections
+            .observableArrayList(
+                    new CodeTable(1, "480p"),
+                    new CodeTable(2, "540p"),
+                    new CodeTable(3, "720p"),
+                    new CodeTable(4, "1080p"));
+
+    public AnimeController(PrintWriter out) {
+        super(out);
+    }
+
+    @FXML
+    public void onActionSave() {
+        logger.warn("Empty onActionSave method");
+    }
+
+    @FXML
+    public void onActionCancel() {
+        logger.debug("Closing scene");
+        Stage stage = (Stage) animeNameTextField.getScene().getWindow();
+        stage.close();
+    }
+
+    public void initChoiceBox() {
+        logger.debug("initializing Choice Box");
+        qualityChoiceBox.getItems().setAll(qualityTypeObservableList);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        logger.trace("Initializing");
+        initChoiceBox();
+    }
+}
