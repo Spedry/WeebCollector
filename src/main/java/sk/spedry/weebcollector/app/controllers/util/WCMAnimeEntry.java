98c364edc2d4d1101eccb1417eb3ccc737e3c2cb
Spedry <spedrysk@gmail.com>
2021-10-19 22:03:23 +0200

    quality string to codetable

--- src/main/java/sk/spedry/weebcollector/app/controllers/util/WCMAnimeEntry.java
+++ src/main/java/sk/spedry/weebcollector/app/controllers/util/WCMAnimeEntry.java
@@ -3,14 +3,16 @@ package sk.spedry.weebcollector.app.controllers.util;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import sk.spedry.weebcollector.app.controllers.choiceboxitems.CodeTable;
 
 public class WCMAnimeEntry {
     @Getter
+    @Setter
     private int id;
     @Getter
     private final String animeName;
     @Getter
-    private final String typeOfQuality;
+    private final CodeTable typeOfQuality;
     @Getter
     private final String botName;
     @Getter
@@ -21,14 +23,14 @@ public class WCMAnimeEntry {
     private String numberOfDownloadedEpisodes;
     // TODO FOR FUTURE private String status;
 
-    public WCMAnimeEntry(@NonNull String anime, @NonNull String typeOfQuality, String botName, String numberOfEpisodes) {
+    public WCMAnimeEntry(@NonNull String anime, @NonNull CodeTable typeOfQuality, String botName, String numberOfEpisodes) {
         this.animeName = anime;
         this.typeOfQuality = typeOfQuality;
         this.botName = botName;
         this.numberOfEpisodes = numberOfEpisodes;
     }
     // TODO DELETE LATER
-    public WCMAnimeEntry(@NonNull String anime, @NonNull String typeOfQuality, String botName, String numberOfEpisodes, String numberOfDownloadedEpisodes) {
+    public WCMAnimeEntry(@NonNull String anime, @NonNull CodeTable typeOfQuality, String botName, String numberOfEpisodes, String numberOfDownloadedEpisodes) {
         this.animeName = anime;
         this.typeOfQuality = typeOfQuality;
         this.botName = botName;
