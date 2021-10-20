a3b0b36fbc6cae9166c6893f394ce65f642a2664
peter <petertokovics@atek.sk>
2021-10-20 09:52:56 +0200

    opens entire module for reflection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,17 @@
-module sk.spedry.weebcollector {
+open module sk.spedry.weebcollector {
     requires javafx.controls;
+    requires javafx.graphics;
     requires javafx.fxml;
-    requires static lombok;
+    requires lombok;
     requires org.apache.logging.log4j;
     requires com.google.gson;
 
     exports sk.spedry.weebcollector to javafx.fxml, javafx.controls, javafx.graphics;
 
+    /*opens sk.spedry.weebcollector.app.controllers.util to com.google.gson;
+
     opens sk.spedry.weebcollector.app.controllers to javafx.fxml;
     opens sk.spedry.weebcollector.app.controllers.cell to javafx.fxml;
     opens sk.spedry.weebcollector.app.controllers.preferencescontrollers to javafx.fxml;
-    opens sk.spedry.weebcollector.app.controllers.util.exteders to javafx.fxml;
+    opens sk.spedry.weebcollector.app.controllers.util.exteders to javafx.fxml;*/
 }
