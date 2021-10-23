c745e8cc1f259c881ddd8fb5a4400c5868ed1716
Spedry <spedrysk@gmail.com>
2021-10-23 19:43:43 +0200

    String numberOfDownloadedEpisodes -> int

--- src/main/java/sk/spedry/weebcollector/app/controllers/util/WCMAnimeEntry.java
+++ src/main/java/sk/spedry/weebcollector/app/controllers/util/WCMAnimeEntry.java
@@ -20,7 +20,7 @@ public class WCMAnimeEntry {
     private String numberOfEpisodes;
     @Getter
     @Setter
-    private String numberOfDownloadedEpisodes;
+    private int numberOfDownloadedEpisodes;
     // TODO FOR FUTURE private String status;
 
     public WCMAnimeEntry(@NonNull String anime, @NonNull CodeTable typeOfQuality, String botName, String numberOfEpisodes) {
@@ -30,7 +30,7 @@ public class WCMAnimeEntry {
         this.numberOfEpisodes = numberOfEpisodes;
     }
     // TODO DELETE LATER
-    public WCMAnimeEntry(@NonNull String anime, @NonNull CodeTable typeOfQuality, String botName, String numberOfEpisodes, String numberOfDownloadedEpisodes) {
+    public WCMAnimeEntry(@NonNull String anime, @NonNull CodeTable typeOfQuality, String botName, String numberOfEpisodes, int numberOfDownloadedEpisodes) {
         this.animeName = anime;
         this.typeOfQuality = typeOfQuality;
         this.botName = botName;
