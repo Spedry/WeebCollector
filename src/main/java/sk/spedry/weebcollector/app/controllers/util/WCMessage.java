c279c61d03cac77b9e06376936fa856ab46bbb3b
Spedry <spedrysk@gmail.com>
2021-12-19 20:24:25 +0100

    added constructor with additionalData

--- src/main/java/sk/spedry/weebcollector/app/controllers/util/WCMessage.java
+++ src/main/java/sk/spedry/weebcollector/app/controllers/util/WCMessage.java
@@ -11,10 +11,19 @@ public class WCMessage {
     @Getter
     @Setter
     private String messageBody;
+    @Getter
+    @Setter
+    private String additionalData;
 
     public <T> WCMessage(String messageId, T object) {
         this.messageId = messageId;
         this.messageBody = new Gson().toJson(object);
+    }
+
+    public <T, F> WCMessage(String messageId, T object, F additionalData) {
+        this.messageId = messageId;
+        this.messageBody = new Gson().toJson(object);
+        this.additionalData = new Gson().toJson(additionalData);
     }
 
     public WCMessage(String messageId) {
