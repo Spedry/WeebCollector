5f97d5d9f656b79bb5751e2f1908ea9123102b26
Spedry <spedrysk@gmail.com>
2021-12-19 20:18:39 +0100

    option to download already released - send number of episodes to download

--- src/main/java/sk/spedry/weebcollector/app/controllers/AddNewAnimeController.java
+++ src/main/java/sk/spedry/weebcollector/app/controllers/AddNewAnimeController.java
@@ -2,9 +2,12 @@ package sk.spedry.weebcollector.app.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import sk.spedry.weebcollector.app.controllers.util.WCMAnimeEntry;
+import sk.spedry.weebcollector.app.controllers.util.WCMDownAlrRel;
 import sk.spedry.weebcollector.app.controllers.util.WCMessage;
 import sk.spedry.weebcollector.app.controllers.util.exteders.AnimeController;
 
@@ -24,17 +27,32 @@ public class AddNewAnimeController extends AnimeController implements Initializa
     @FXML
     @Override
     public void onActionSave() {
-        String botName = null, numberOfEp = null;
+        String botName = null;
+        int numberOfEp = 0, numberOfEpToDownload = 0;
         if (!numberOfEpisodesTextField.getText().isEmpty())
-            numberOfEp = numberOfEpisodesTextField.getText();
+            numberOfEp = Integer.parseInt(numberOfEpisodesTextField.getText());
         if (!botTextField.getText().isEmpty())
             botName = botTextField.getText();
 
-        sendMessage(new WCMessage("addNewAnimeEntry", new WCMAnimeEntry(
-                animeNameTextField.getText(),
-                qualityChoiceBox.getValue(),
-                botName,
-                numberOfEp)));
+        if (check) {
+            if (!numberOfEpisodesTextField2.getText().isEmpty())
+                numberOfEp = Integer.parseInt(numberOfEpisodesTextField2.getText());
+            if (!numberOfEpisodesToDownloadTextField.getText().isEmpty())
+                numberOfEpToDownload = Integer.parseInt(numberOfEpisodesToDownloadTextField.getText());
+            sendMessage(new WCMessage("addNewAnimeEntry", new WCMAnimeEntry(
+                    animeNameTextField.getText(),
+                    qualityChoiceBox.getValue(),
+                    botName,
+                    numberOfEp),
+                    new WCMDownAlrRel(check, numberOfEpToDownload)));
+        }
+        else {
+            sendMessage(new WCMessage("addNewAnimeEntry", new WCMAnimeEntry(
+                    animeNameTextField.getText(),
+                    qualityChoiceBox.getValue(),
+                    botName,
+                    numberOfEp)));
+        }
         //TODO
         // ADD OPTIONS TO CHOOSE IF USER WANT TO CLOSE STAGE
         // AFTER ADDING NEW ANIME ENTRY
@@ -46,5 +64,6 @@ public class AddNewAnimeController extends AnimeController implements Initializa
         logger.trace("Initializing");
         initChoiceBox();
         setHandler();
+        hBox.setVisible(false);
     }
 }
