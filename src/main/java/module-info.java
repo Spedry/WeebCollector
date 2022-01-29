8c219471baf9cc37466736ccf8524f82135d50d0
Spedry <spedrysk@gmail.com>
2022-01-29 19:22:23 +0100

    added java.desktop for opening files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,9 @@ open module sk.spedry.weebcollector {
     requires lombok;
     requires org.apache.logging.log4j;
     requires com.google.gson;
+    requires java.desktop;
 
     exports sk.spedry.weebcollector to javafx.fxml, javafx.controls, javafx.graphics;
-
     /*opens sk.spedry.weebcollector.app.controllers.util to com.google.gson;
 
     opens sk.spedry.weebcollector.app.controllers to javafx.fxml;
